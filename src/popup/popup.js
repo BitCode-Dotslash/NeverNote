10fba7130079a256ef33c0e9c2bb1b19db54175b
shrushti22 <shrushtivasaniya@gmail.com>
2022-01-08 01:43:40 +0530

    add- create notebook fuctionality in popup

---
+++ src/popup/popup.js
@@ -0,0 +1,29 @@
+
+// display create notebook section on click create notebook button
+$("#createNotebook").on("click", function(){
+    $("#createNotebookDiv").css("display", "block");
+    $("#createNotebookDiv #createNotebookForm button").prop("disabled", true);
+})
+
+
+//check for valid notebook name (unique name)
+$("#createNotebookDiv input#notebookName").keyup(function(){
+    var inputText = $(this).val();
+    // console.log(inputText);
+    chrome.storage.sync.get(['notes'], function(result) {
+        var notebooks = Object.keys(result.notes);
+        var found = notebooks.indexOf(inputText);
+        // console.log(found);
+        if(found==-1){
+            $("#createNotebookDiv input#notebookName").css({"border": "5px solid green"});
+            $("#createNotebookDiv input#notebookName").css({"background-color": "green"});
+            $("#createNotebookDiv #createNotebookForm button").prop("disabled", false);
+            // console.log("undefined");
+        }else{
+            $("#createNotebookDiv input#notebookName").css({"border": "5px solid red"});
+            $("#createNotebookDiv input#notebookName").css({"background-color": "red"});
+            $("#createNotebookDiv #createNotebookForm button").prop("disabled", true);
+            // console.log("defined");
+        }
+    })
+});
